691f6158ae05a5dec5ee913535ee8c65363a4a81
josemm0505 <mejiamicoltalol@gmail.com>
2022-04-29 16:01:28 -0500

    Programa para calcular la edad de una persona

---
+++ MejiaJose/ACTIVIDAD-B2/MejiaJose-laedad.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int dd, mm, yy, dd1, mm1, yy1, da, ma, ya;
+	cout<<"Ingrese la fecha actual en el formato dd/mm/yy: "<<endl;
+	cin>>dd>>mm>>yy;
+	cout<<"Ingrese su fecha de nacimiento en el formato dd/mm/yy: "<<endl;
+	cin>>dd1>>mm1>>yy1;
+	if (dd<dd1){
+		dd=dd+30;
+		mm=mm-1;
+		da=dd-dd1;
+	}
+		else {
+			da=dd-dd1;
+		}
+	if (mm<mm1){
+		mm=mm+12;
+		yy=yy-1;
+		ma=mm-mm1;
+	}
+		else {
+			ma=mm-mm1;
+		}
+	ya=yy-yy1;
+	cout<<"Usted tiene "<<ya<<" anios, "<<ma<<" meses "<<"y "<<da<<" dias."<<endl;
+	return 0;
+}
