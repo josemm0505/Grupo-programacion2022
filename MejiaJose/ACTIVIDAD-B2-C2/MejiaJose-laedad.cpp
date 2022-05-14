d2fbdb14448f304aeca1334565dd4859da275523
josemm0505 <mejiamicoltalol@gmail.com>
2022-05-14 00:52:23 -0500

    Segunda Versión, Edad de una persona

--- MejiaJose/ACTIVIDAD-B2-C2/MejiaJose-laedad.cpp
+++ MejiaJose/ACTIVIDAD-B2-C2/MejiaJose-laedad.cpp
@@ -1,29 +1,49 @@
+//================================================
+
+//==> Nombre del programa: Edad de una persona
+
+//==> Archivo : MejiaJose-laedad.cpp
+//==>Autor: Jose Mejia
+//==>Fecha de elaboración: 2022-04-29
+//==>Fecha ultima actualización: 2022-05-13
+//=================================================
+
+
 #include <iostream>
 using namespace std;
 int main ()
 {
-	int dd, mm, yy, dd1, mm1, yy1, da, ma, ya;
+	int jm_dd, jm_mm, jm_yy, jm_dd1, jm_mm1, jm_yy1, jm_da, jm_ma, jm_ya;
 	cout<<"Ingrese la fecha actual en el formato dd/mm/yy: "<<endl;
-	cin>>dd>>mm>>yy;
+	cin>>jm_dd>>jm_mm>>jm_yy;
 	cout<<"Ingrese su fecha de nacimiento en el formato dd/mm/yy: "<<endl;
-	cin>>dd1>>mm1>>yy1;
-	if (dd<dd1){
-		dd=dd+30;
-		mm=mm-1;
-		da=dd-dd1;
+	cin>>jm_dd1>>jm_mm1>>jm_yy1;
+	if (jm_dd<jm_dd1){
+		jm_dd=jm_dd+30;
+		jm_mm=jm_mm-1;
+		jm_da=jm_dd-jm_dd1;
 	}
 		else {
-			da=dd-dd1;
+			jm_da=jm_dd-jm_dd1;
 		}
-	if (mm<mm1){
-		mm=mm+12;
-		yy=yy-1;
-		ma=mm-mm1;
+	if (jm_mm<jm_mm1){
+		jm_mm=jm_mm+12;
+		jm_yy=jm_yy-1;
+		jm_ma=jm_mm-jm_mm1;
 	}
 		else {
-			ma=mm-mm1;
+			jm_ma=jm_mm-jm_mm1;
 		}
-	ya=yy-yy1;
-	cout<<"Usted tiene "<<ya<<" anios, "<<ma<<" meses "<<"y "<<da<<" dias."<<endl;
+	jm_ya=jm_yy-jm_yy1;
+
+cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: Edad de una persona"<<endl;
+cout<<"//==> Archivo : MejiaJose-laedad.cpp"<<endl;
+cout<<"//==>Autor: Jose Mejia"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-04-29"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-13"<<endl;
+cout<<"//================================================="<<endl;
+
+	cout<<"Usted tiene "<<jm_ya<<" anios, "<<jm_ma<<" meses "<<"y "<<jm_da<<" dias."<<endl;
 	return 0;
 }
