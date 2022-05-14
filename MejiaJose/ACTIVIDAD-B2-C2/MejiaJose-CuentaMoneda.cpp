c67b842807e3141dc54805e41da31876c46deec7
josemm0505 <mejiamicoltalol@gmail.com>
2022-05-14 00:28:52 -0500

    Segunda Versión, Cuenta Monedas

--- MejiaJose/ACTIVIDAD-B2-C2/MejiaJose-CuentaMoneda.cpp
+++ MejiaJose/ACTIVIDAD-B2-C2/MejiaJose-CuentaMoneda.cpp
@@ -1,28 +1,47 @@
+//================================================
+
+//==> Nombre del programa: Contador de Monedas
+//==> Archivo : MejiaJose-CuentaMoneda.cpp
+//==>Autor: Jose Mejia
+//==>Fecha de elaboración: 2022-04-29
+//==>Fecha ultima actualización: 2022-05-13
+//=================================================
+
 #include <iostream>
 using namespace std;
 int main ()
 {
-	int n, i, sum=0, sum1=0, sum25=0, sum50=0;
+	int jm_n, jm_i, jm_sum=0, jm_sum1=0, jm_sum25=0, jm_sum50=0;
 	cout<<"Ingrese la cantidad de monedas que desea depositar: "<<endl;
-	cin>>n;
-	int num [n];
+	cin>>jm_n;
+	int num [jm_n];
 	cout<<"Ingrese el valor de las monedas: "<<endl;
-	for (int i=0; i<n; i++){
-		cin>>num [i];
-		sum=sum+num [i];
-		if (num [i]==10){
-			sum1=sum1+(num[i]==10);	
+	for (int jm_i=0; jm_i<jm_n; jm_i++){
+		cin>>num [jm_i];
+		jm_sum=jm_sum+num [jm_i];
+		if (num [jm_i]==10){
+			jm_sum1=jm_sum1+(num[jm_i]==10);	
 	}
-		else if (num [i]==25){
-			sum25=sum25+(num[i]==25);
+		else if (num [jm_i]==25){
+			jm_sum25=jm_sum25+(num[jm_i]==25);
 	}
 		else {
-			sum50=sum50+(num[i]==50);
+			jm_sum50=jm_sum50+(num[jm_i]==50);
 	}
+
 }
-	cout<<"Monedas de 50 centavos: "<<sum50<<endl;
-	cout<<"Monedas de 25 centavos: "<<sum25<<endl;
-	cout<<"Monedas de 10 centavos: "<<sum1<<endl;
-	cout<<"La cantidad total de dinero es: "<<sum<<endl;
+
+cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: Contador de Monedas"<<endl;
+cout<<"//==> Archivo : MejiaJose-CuentaMoneda.cpp"<<endl;
+cout<<"//==>Autor: Jose Mejia"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-04-29"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-13"<<endl;
+cout<<"//================================================="<<endl;
+
+	cout<<"Monedas de 50 centavos: "<<jm_sum50<<endl;
+	cout<<"Monedas de 25 centavos: "<<jm_sum25<<endl;
+	cout<<"Monedas de 10 centavos: "<<jm_sum1<<endl;
+	cout<<"La cantidad total de dinero es: "<<jm_sum<<endl;
 return 0;
 }
