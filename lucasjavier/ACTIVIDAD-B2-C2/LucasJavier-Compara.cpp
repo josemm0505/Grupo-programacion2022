e809ec1e8df22b40c0ffb339ddd9dbec455055d5
javierlu25 <javierlucasangulo@gmail.com>
2022-05-19 12:25:41 -0500

    COMPARACION DE DOS NUMEROS-SEGUNDA VERSION

--- lucasjavier/ACTIVIDAD-B2-C2/LucasJavier-Compara.cpp
+++ lucasjavier/ACTIVIDAD-B2-C2/LucasJavier-Compara.cpp
@@ -1,20 +1,38 @@
+//=================================================
+
+//==> Nombre del programa: Comparacion dos numeros
+
+//==> Archivo : LucasJavier-Compara.cpp
+//==>Autor: Javier Lucas
+//==>Fecha de elaboración: 2022-04-23
+//==>Fecha ultima actualización: 2022-05-19
+//==================================================
+
 #include <iostream>
 using namespace std;
 int main()
 {
-	float J,K;
-	cout<<"Escribir el primer valor: "<<endl;
-	cin>>J;
-	cout<<"Escribir el segundo valor: "<<endl;
-	cin>>K;
-	if (J==K){
-	cout<<"El primer valor es igual al segundo valor"<<endl;
+	float LJ_J,LJ_K;
+	cout<<"Digite un valor: "<<endl;
+	cin>>LJ_J;
+	cout<<"Digite un valor: "<<endl;
+	cin>>LJ_K;
+
+	if (LJ_J==LJ_K){
+	cout<<"Los dos valores son iguales"<<endl;
 	}
-	else if (J>K){
-		cout<<"El valor "<<J<<" es mayor que "<<K<<endl;
+	else if (LJ_J>LJ_K){
+		cout<<"El valor "<<LJ_J<<" es mayor que "<<LJ_K<<endl;
 	}
 	else{
-		cout<<"El valor "<<K<<" es mayor que "<<J<<endl;
+		cout<<"El valor "<<LJ_K<<" es mayor que  "<<LJ_J<<endl;
 	} 
+	cout<<endl<<"//================================================="<<endl;
+cout<<"//==> Nombre del programa: Comparacion dos numeros"<<endl;
+cout<<"//==> Archivo : LucasJavier-Compara.cpp"<<endl;
+cout<<"//==>Autor: Javier Lucas"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-04-23"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-19"<<endl;
+cout<<"//=================================================="<<endl;
 	return 0;
 }
