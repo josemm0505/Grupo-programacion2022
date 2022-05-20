9a8e04320d70e6d49ae5b8af39b3a8b282373412
EmikoIbarra <emiko.ibarra.herrera@utelvt.edu.ec>
2022-05-20 18:45:04 -0500

    Modificacion de Cuenta Moneda

--- IbarraEmiko/ACTIVIDAD-B2-C2/IbarraEmiko-CuentaMoneda.cpp
+++ IbarraEmiko/ACTIVIDAD-B2-C2/IbarraEmiko-CuentaMoneda.cpp
@@ -1,30 +1,44 @@
+//=============================================
+//==> Nombre del programa: Sacar cuentas de monedas
+//==> Archivo: IbarraEmiko-CuentaMoneda.cpp
+//==> Autora: Emiko Ibarra
+//==> Fecha de elaboración: 20/05/2022
+//==> Fecha de modificación: 20/05/2022
+//=============================================
 #include <iostream>
 using namespace std ;
 int  main()
 {
-	int a, b= 0 , b1= 0 , b2= 0 ;
-	float x, m= 0 , m1= 0 , m2= 0 ;
+	int ei_a, ei_b= 0 , ei_b1= 0 , ei_b2= 0 ;
+	float ei_x, ei_m= 0 , ei_m1= 0 ,ei_m2= 0 ;
 	cout<< "Ingreso de la cantidad de monedas: " ;
-	cin>>a;
+	cin>>ei_a;
 	do {
 		cout<< " Ingrese el valor de las monedas (0.10,0.25): " ;
-		cin>>x;
-		b=b+ 1 ;
-		m=m+x;
-		if (x== 0.10 ){
-			b1=b1+ 1 ;
-			m1=m1+x;
+		cin>>ei_x;
+		ei_b=ei_b+ 1 ;
+		ei_m=ei_m+ei_x;
+		if (ei_x== 0.10 ){
+			ei_b1=ei_b1+ 1 ;
+			ei_m1=ei_m1+ei_x;
 		} else {
-		b2=b2+ 1 ;
-		m2=m2+x;
+		ei_b2=ei_b2+ 1 ;
+		ei_m2=ei_m2+ei_x;
 		}
-	} while (b<a);
+	} while (ei_b<ei_a);
 	cout<< " Resultado: " <<endl;
-	cout<< "La cantidad de monedas ingresadas es: " <<b<<endl;
-	cout<< "El total de dinero contado es: " <<m<<endl;
-	cout<< " La antidad de monedas de 0.10c ingresadas es: " <<b1<<endl;
-	cout<< " Total de dinero en monedas de 0.10c: " <<m1<<endl;
-	cout<< "Cantidad de monedas de 0,25c ingresadas: " <<b2<<endl;
-	cout<< "La cantidad de dinero en monedas de 0.25c es: " <<m2<<endl;
+	cout<< "La cantidad de monedas ingresadas es: " <<ei_b<<endl;
+	cout<< "El total de dinero contado es: " <<ei_m<<endl;
+	cout<< " La antidad de monedas de 0.10c ingresadas es: " <<ei_b1<<endl;
+	cout<< " Total de dinero en monedas de 0.10c: " <<ei_m1<<endl;
+	cout<< "Cantidad de monedas de 0,25c ingresadas: " <<ei_b2<<endl;
+	cout<< "La cantidad de dinero en monedas de 0.25c es: " <<ei_m2<<endl;
+cout<<endl<<"============================================="<<endl;
+cout<<"//==>Nombre del programa: Sacar cuentas de monedas"<<endl;
+cout<<"//==>Archivo: IbarraEmiko-CuentaMoneda.cpp "<<endl;
+cout<<"//==>Autora: Emiko Ibarra "<<endl;
+cout<<"//==>Fecha de elaboración: 20/05/2022 "<<endl;
+cout<<"//==>Fecha de modificación: 20/05/2022 "<<endl;
+cout<<"//============================================= "<<endl;
 	return  0 ;
 }
