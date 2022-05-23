93d664134e13745863c62daaa5459b21d42fb038
DannaCapurroMendoza <danna.capurro.mendoza@utelvt.edu.ec>
2022-05-23 10:03:47 -0500

    Libreria finalizada_dc

--- ACTIVIDAD-A2/dc_libreria.h
+++ ACTIVIDAD-A2/dc_libreria.h
@@ -2,7 +2,7 @@
 int Dc_Compara ()
 {
 	
-	float Dc_valor1, Dc_valor2;
+	float Dc_valor1, Dc_valor2, flag1;
 	cout<< " \n //==> Programa para comprar dos numeros <==// \n  " <<endl;
 	cout<< " Ingrese un valor: " <<endl;
 	cin>>Dc_valor1;
@@ -12,7 +12,7 @@ int Dc_Compara ()
 	if (Dc_valor1==Dc_valor2){
 	cout<< " Ambas valores son iguales " <<endl;
 	}
-	 if(Dc_valor1>Dc_valor2) {
+	else if(Dc_valor1>Dc_valor2) {
 	cout<< " El valor " <<Dc_valor2<< " es mayor que " <<Dc_valor1<<endl;
 	       }
 	else{
@@ -35,7 +35,7 @@ cout<< "\n Press Enter to exit. \n"<<endl;
 // Programa para sumar varios numeros
 int  Dc_SumaN ()
 {
-	int Dc_c= 0 , Dc_n;
+	int Dc_c= 0 , Dc_n, flag1;
 	float Dc_s= 0 , Dc_x;
 	cout<< " \n //==> Programa para sumar varios numeros <==// \n  " <<endl;
 	cout<< " Ingrese la cantidad de elementos a sumar: " <<endl;
@@ -65,29 +65,27 @@ cout<< "\n Press Enter to exit. \n"<<endl;
 // Programa Punto Venta
 int  Dc_PuntoVenta ()
 {
-	int Dc_C= 0 ,Dc_B;
-	float Dc_A= 0 ,Dc_x,Dc_Tb,Dc_PIVA,Dc_Pdsc,Dc_IVA= 0.12 ,Dc_dsc= 0.30 ,Dc_VT;
-	cout<< " \n //==> Programa para conocer el Punto de Venta <==// \n  " <<endl;
-	cout<< " Ingrese Cantidad de elementos a comprar: " <<endl;
+	int Dc_C=0, Dc_B, flag1;
+	float Dc_A=0, Dc_X, Dc_s, Dc_Vdsc, Dc_V1, Dc_Ivav, Dc_Fn;
+	cout<<"\n //==> Programa para conocer el Punto de Venta <==//\n "<<endl;
+	cout<<"Ingrese el valor de los productos a sumar"<<endl;
 	cin>>Dc_B;
 	do{
-		cout<< " Ingrese el valor del elemento: " <<Dc_C+1 <<endl;
-		cin>>Dc_x;
-		Dc_C=Dc_C+ 1 ;
-		Dc_A=Dc_A+Dc_x;
-		Dc_PIVA=Dc_A*Dc_IVA;
-		cout<< " El valor del IVA de su compra es:$ " <<Dc_PIVA<<endl;
-		Dc_Pdsc=Dc_A*Dc_dsc;
-		cout<< " El valor del descuento de su compra es:$ " <<Dc_Pdsc<<endl;
-		Dc_Tb=Dc_PIVA+Dc_Pdsc;
-	} while (Dc_C<Dc_B);
-	if (Dc_Tb==Dc_A){
-cout<< " Su compra aplica a Descuento: " <<Dc_Tb<<endl;
-		Dc_IVA=Dc_Tb*Dc_PIVA/ 100 ;
-		Dc_dsc=Dc_Tb*Dc_Pdsc/ 100 ;
-	} else {
-		Dc_VT=Dc_A+Dc_PIVA-Dc_Pdsc;
-cout<< " El valor total a pagar es:$ " <<Dc_VT<<endl;
+		cout<<"Ingrese el valor de su compra "<<Dc_C+1<<endl;
+		cin>>Dc_X;
+		Dc_C=Dc_C+1;
+		Dc_A=Dc_A+Dc_X;
+	}while (Dc_C<Dc_B);
+	cout<<"Ingrese el Valor del Descuento: "<<endl;
+	cin>>Dc_s;
+	cout<<"Valor Bruto de Compra: "<<Dc_A<<endl;
+	Dc_Vdsc=(Dc_A*Dc_X)/100;
+	cout<<"Valor de su Descuento: "<<Dc_Vdsc<<endl;
+	Dc_V1=Dc_A-Dc_Vdsc;
+	Dc_Ivav=Dc_V1*0.12;
+	cout<<"Valor Obtenido del IVA: "<<Dc_Ivav<<endl;
+	Dc_Fn=(Dc_A-Dc_Vdsc)+Dc_Ivav;
+	cout<<"Valor Final: "<<Dc_Fn<<endl;
 cout<<"\n//======================================================="<<endl;
 cout<<"//========> Nombre del programa: Punto de Venta <========"<<endl;
 cout<<"//=========> Archivo : CapurroDanna-PuntoVenta.cpp <========"<<endl;
@@ -99,44 +97,36 @@ cout<< "\n Press Enter to exit. \n"<<endl;
 	flag1=cin.ignore().get();
 return 0;
 }
-
-	
-<<<<<<< HEAD
-=======
-	}
-	return  0 ;
-}
->>>>>>> 7bebc36400de51d50fea236bfb50edc29a5d0f5f
 	
 	
 // Programa laedad
 int  Dc_laedad ()
 {
-int Dc_dd,Dc_mm,Dc_aa,Dc_dd1,Dc_mm1,Dc_aa1,Dc_yy1,Dc_da,Dc_ma,Dc_yy,Dc_ya;
-	cout<< " \n //==> Programa para saber la edad exacta de una persona <==// \n  " <<endl;
-	cout<< " INGRESE LA FECHA ACTUAL: Dia Mes AñO " ;
+	int Dc_dd, Dc_mm, Dc_aa, Dc_dd1, Dc_mm1, Dc_aa1, Dc_da, Dc_ma, Dc_ya, flag1;
+	cout<<"\n //==> Programa para saber la edad exacta de una persona <==//\n "<<endl;
+	cout<<"INGRESE LA FECHA ACTUAL: dd/mm/yy: "<<endl;
 	cin>>Dc_dd>>Dc_mm>>Dc_aa;
-	cout<< " INGRESE SU FECHA DE NACIMIENTO: Dia Mes Año " ;
-	cin>>Dc_dd1>>Dc_mm1>>Dc_yy1 ;
+	cout<<"INGRESE SU FECHA DE NACIMIENTO: dd/mm/yy: "<<endl;
+	cin>>Dc_dd1>>Dc_mm1>>Dc_aa1;
 	if (Dc_dd<Dc_dd1){
-		Dc_dd=Dc_dd+ 30 ;
-		Dc_mm=Dc_mm- 1 ;
+		Dc_dd=Dc_dd+30;
+		Dc_mm=Dc_mm-1;
 		Dc_da=Dc_dd-Dc_dd1;
 	}
-	   else {
-		Dc_da=Dc_dd-Dc_dd1;
-     	}
+	        else {
+			Dc_da=Dc_dd-Dc_dd1;
+		}
 	if (Dc_mm<Dc_mm1){
-		Dc_mm=Dc_mm+ 12 ;
-		Dc_yy=Dc_yy- 1 ;
+		Dc_mm=Dc_mm+12;
+		Dc_aa=Dc_aa-1;
 		Dc_ma=Dc_mm-Dc_mm1;
 	}
-	else {
-		Dc_ma=Dc_mm-Dc_mm1;
-	}
-	Dc_ya=Dc_yy-Dc_yy1;
-	
-cout<< " USTED TIENE: " <<Dc_ya<< " Años; " <<Dc_ma<< " Meses " << " y " <<Dc_da<< " Dia " <<endl;
+		else {
+			Dc_ma=Dc_mm-Dc_mm1;
+		}
+	Dc_ya=Dc_aa-Dc_aa1;
+
+cout<<"USTED TIENE: "<<Dc_ya<<" Años, "<<Dc_ma<<" Meses "<<"y "<<Dc_da<<" Dias."<<endl;
 cout<<"\n//======================================================="<<endl;
 cout<<"//======> Nombre del programa: Edad de una persona <====="<<endl;
 cout<<"//===========> Archivo : CapurroDanna-laedad.cpp <=========="<<endl;
@@ -154,7 +144,7 @@ cout<< "\n Press Enter to exit. \n"<<endl;
 // Programa CuentaMoneda
 int  Dc_CuentaMoneda ()
 {
-	int Dc_n, Dc_c= 0 , Dc_c1= 0 , Dc_c2= 0 ;
+	int Dc_n, Dc_c= 0 , Dc_c1= 0 , Dc_c2= 0 , flag1;
 	float Dc_x,Dc_a= 0 , Dc_a1= 0 , Dc_a2= 0 ;
 	cout<< " \n //==> Programa para contar y clasificar monedas <==// \n  " <<endl;
 	cout<< "CANTIDAD DE MONEDAS A INGRESAR :" <<endl;
@@ -231,7 +221,7 @@ int  Dc_menu ()
 					break ;
 				}
 				if (op!= 0 && op> 5 ){
-				   cout << " \n Opcion no valida \n " <<   " Presiona Enter para continuar \n " ;	    
+				   cout << " \n Opcion no valida \n " <<   " Presiona Enter para continuar \n";	    
 				  flag1=cin.ignore().get();
 				  }		  
 				if(op==0){
